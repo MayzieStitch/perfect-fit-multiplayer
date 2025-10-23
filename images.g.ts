496d0f934ed504c129047cdadffa7dcfde8a7553
MayzieStitch <30ya101098@heights.qld.edu.au>
2025-10-23 14:14:27 +1000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,42 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Horatio":return img`
+. . b b b b b . . 
+. f b 2 2 2 b f . 
+. . b b b b b . . 
+. . b b b b b . . 
+. . b b b b b . . 
+f . . f f f . . f 
+b b b b b b b b b 
+b b b 6 6 6 b b b 
+b b 6 6 6 9 6 b b 
+b b 6 6 b 6 6 b b 
+9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 
+b b b b b b b b b 
+b b b b b b b b b 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
